83da072579e6f0379d3086df4b9c2fb690720575
codwarrior <108271839+codwarrior@users.noreply.github.com>
2024-01-29 13:50:32 -0500

    Add a clearnet notice and a conditional in getcat.js

--- js/getcat.js
+++ js/getcat.js
@@ -1,4 +1,11 @@
+const enabled = true
+
 $(document).ready(function() { // ONLY when the page is done loading.
+    // so i can use nvim live view without ddosing TheCatAPI. enabled should be set to true
+    // when pushed to neocities.
+    if (enabled == false) {
+        return;
+    }
     // make a request to thecatapi.com to get a single cat image
     $.get('https://api.thecatapi.com/v1/images/search?limit=1', function(data) { 
         // then make a new element inside of div cat-images and set an img src to index 0 dot urll
