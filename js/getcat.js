b3ef9197e9d0992d3332893384606d18953ad1e8
bob <108271839+minegamer72@users.noreply.github.com>
2024-04-20 15:24:18 -0400

    correct comment

--- js/getcat.js
+++ js/getcat.js
@@ -8,7 +8,7 @@ $(document).ready(function() { // ONLY when the page is done loading.
     }
     // make a request to thecatapi.com to get a single cat image
     $.get('https://api.thecatapi.com/v1/images/search?limit=1', function(data) { 
-        // then make a new element inside of div cat-images and set an img src to index 0 dot urll
+        // then add it to the #cat-images element
          $('#cat-images').append('<img src="' + data[0].url + '" class="cat">'); 
      });
 });
