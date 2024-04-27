59b95e05c688b27e7cabde7115722c548b746983
bob <bob@bobsdomain.com>
2024-04-27 14:59:49 -0400

    add opinion page template

---
+++ pages/rambles/js/showText.js
@@ -0,0 +1,9 @@
+// @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3-or-Later
+function showText() {
+    var text = document.getElementById("text");
+    var warning = document.getElementById("opinionWarning");
+    
+    warning.style.display = "none";
+    text.style.display = "block";
+}
+// @license-end
